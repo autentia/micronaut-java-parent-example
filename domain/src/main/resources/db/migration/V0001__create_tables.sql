2fb05c0d4cd1afd77e0f85df8d709affdfc382fa
Alejandro Pérez García <alejandropg@autentia.com>
2021-04-23 14:24:41 +0200

    Moved to io.archimedesfw

--- domain/src/main/resources/db/migration/V0001__create_tables.sql
+++
@@ -1,8 +0,0 @@
-create table Todo
-(
-    id           integer primary key generated always as identity,
-    title        varchar(255) not null,
-    creation_date timestamp    not null,
-    done_date     timestamp    null
-);
-create index idx_Todo_creationDate on Todo (creation_date);
